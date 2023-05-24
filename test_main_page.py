46c3dfd38808bef4b4552e380ea5dcd45f07993a
Alexey Bobrikov <a.beaverg@gmail.com>
2023-05-25 00:20:14 +0300

    upload login page first tests

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,10 @@
 """
-Run test file:
+Run test file in cmd-line:
     pytest -v --tb=line --language=en test_main_page.py
 
 --tb=line -> Write only one string in console from logs of failed tests
 """
+from pages.login_page import LoginPage
 from pages.main_page import MainPage
 
 
@@ -19,3 +20,24 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_guest_should_see_login_url_on_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+
+
+def test_guest_should_see_login_form_on_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+
+def test_guest_should_see_register_form_on_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
