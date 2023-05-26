2b2715080a664b6aacf9c2051d51a21b0b8ad764
Alexey Bobrikov <a.beaverg@gmail.com>
2023-05-26 12:28:29 +0300

    upload product page test

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,11 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.ID, "login_form")
     REG_FORM = (By.ID, "register_form")
+
+
+class ProductPageLocators:
+    TITLE_WITH_PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    ADD_TO_BASKET_BTN = (By.CLASS_NAME, "btn-add-to-basket")
+    MESSAGE_WITH_NAME_OF_ADDED_PRODUCT = (By.CSS_SELECTOR, "#messages .alert:nth-child(1) strong")
+    TITLE_WITH_PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    MESSAGE_WITH_PRICE_OF_ADDED_PRODUCT = (By.CSS_SELECTOR, "#messages .alert:nth-child(3) strong")
---
+++ pages/product_page.py
@@ -0,0 +1,22 @@
+from pages.base_page import BasePage
+from pages.locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_product_to_basket(self):
+        add_product_btn = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BTN)
+        add_product_btn.click()
+
+    def should_be_correct_product_name_in_message(self):
+        product_name_in_title = self.browser.find_element(*ProductPageLocators.TITLE_WITH_PRODUCT_NAME).text
+        product_name_in_message = self.browser\
+            .find_element(*ProductPageLocators.MESSAGE_WITH_NAME_OF_ADDED_PRODUCT).text
+        assert product_name_in_title == product_name_in_message, \
+            "Product name in title is not equal product name in message"
+
+    def should_be_correct_product_price_in_message(self):
+        product_price_in_title = self.browser.find_element(*ProductPageLocators.TITLE_WITH_PRODUCT_PRICE).text
+        product_price_in_message = self.browser\
+            .find_element(*ProductPageLocators.MESSAGE_WITH_PRICE_OF_ADDED_PRODUCT).text
+        assert product_price_in_title == product_price_in_message, \
+            "Product price in title is not equal product name in message"
