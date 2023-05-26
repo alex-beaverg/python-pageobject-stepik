9b6e042159ccc0c45f7341449286f59954fbcc8b
Alexey Bobrikov <a.beaverg@gmail.com>
2023-05-26 13:08:28 +0300

    upload finding messages tests

--- pages/product_page.py
+++ pages/product_page.py
@@ -3,6 +3,14 @@ from pages.locators import ProductPageLocators
 
 
 class ProductPage(BasePage):
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.MESSAGE_WITH_NAME_OF_ADDED_PRODUCT), \
+            "Success message is presented, but should not be"
+
+    def should_disappear_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.MESSAGE_WITH_NAME_OF_ADDED_PRODUCT), \
+            "Success message is not disappeared, but should disappear"
+
     def add_product_to_basket(self):
         add_product_btn = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BTN)
         add_product_btn.click()
--- test_product_page.py
+++ test_product_page.py
@@ -7,6 +7,32 @@ import pytest
 from pages.product_page import ProductPage
 
 
+@pytest.mark.xfail(reason="test should fail")
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.xfail(reason="test should fail")
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.should_disappear_success_message()
+
+
+@pytest.mark.skip
 @pytest.mark.parametrize('link', [
     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
